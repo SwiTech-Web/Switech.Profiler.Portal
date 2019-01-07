a5aa731f2046e2c850ce2db15643d821c6d2775a
Julien <julien1105@outlook.fr>
2019-01-07 12:34:15 +0100

    correction procjet details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
 import { ServicesComponent } from './services/services.component';
 import { JobDocumentationComponent } from './job-documentation/job-documentation.component';
+import { ProjectDetailsComponent } from './project/project-details/project-details.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'project', component: ProjectComponent },
+  { path: 'project-details', component: ProjectDetailsComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'job-documentation', component: JobDocumentationComponent },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
